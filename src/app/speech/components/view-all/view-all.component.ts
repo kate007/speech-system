99302aa14e997c0a240310c89c253ffb2462ebf4
Kate.G <kate.geronimo@didgigo.com.au>
2020-05-03 19:24:20 +0800

    View All - add scrolling

--- src/app/speech/components/view-all/view-all.component.ts
+++ src/app/speech/components/view-all/view-all.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
 import { SpeechService } from '../../services/speech.service';
 import { Speech } from '../../models/speech.model';
 import { ActivatedRoute } from '@angular/router';
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './view-all.component.html',
   styleUrls: ['./view-all.component.css']
 })
-export class ViewAllComponent implements OnInit {
+export class ViewAllComponent implements OnInit, AfterViewInit {
 
 
   speechList: Speech[] = [];
@@ -16,7 +16,7 @@ export class ViewAllComponent implements OnInit {
   selectedIndex:number = 0;
   bgColor:string;
   
-  constructor(private speechService:SpeechService, private route:ActivatedRoute) { }
+  constructor(private speechService:SpeechService, private route:ActivatedRoute, private renderer:Renderer2) { }
 
   ngOnInit() {                                      
     this.selectedIndex = +this.route.snapshot.params.id || 0 ;     
@@ -27,6 +27,14 @@ export class ViewAllComponent implements OnInit {
     } else {
         this.selectedIndex = 0;
     }   
+  
+  }
+
+  ngAfterViewInit()
+  {
+    const element = this.renderer.selectRootElement('#speechList-' + (this.selectedIndex).toString(), true);
+    element.scrollIntoView({ behavior: 'smooth' });
+    //this.initScroll(0, this.selectedIndex);  
   }
 
   setSelectedIndex(i:number )
@@ -48,6 +56,33 @@ export class ViewAllComponent implements OnInit {
     {
       this.setSelectedIndex(0);
     }  
+  }
+
+   
+  initScroll( offset: number, id: number): void {
+    const element = document.querySelector('#speechList-' + id.toString());
+  
+     // scroll the entry
+     if (element) {
+      const yCoordinate = element.getBoundingClientRect().top + window.pageYOffset;
+      console.log(yCoordinate);
+      const yOffset = -1 * offset;
+      const isIEOrEdge = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent); // check if edge because scrollTO to does not work on edge
+
+      if (!isIEOrEdge) {
+        console.log('scrollTo');
+          window.scrollTo({
+              top: 500, /* yCoordinate + yOffset, */
+            behavior: 'smooth',
+          });
+       } else {
+        element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'start'});
+        const scrolledY = window.scrollY;
+        if (scrolledY) {
+            window.scroll(0, scrolledY + yOffset);
+          }
+       }
+    }
   }
 
 }
--- src/app/speech/services/speech.service.ts
+++ src/app/speech/services/speech.service.ts
@@ -15,15 +15,17 @@ export class SpeechService {
   addInitSpeeches()
   {   
   
-    this.add(new Speech( 'Winston Churchill', 'You ask, what is our aim? I can answer in one word: Victory. Victory at all costs—Victory in spite of all terror—Victory, however long and hard the road may be, for without victory there is no survival.'));
-    this.add(new Speech( 'Winston Churchill', '[The House of Lords] regards all our liberties and political rights as enjoyed and enjoyable only so long as they choose to let us go on having them. But once we touch reality, once we touch their interests and privileges - [kicks his platform] Out!'));
-    this.add(new Speech( 'Winston Churchill', 'The great air battle which has been in progress over this Island for the last few weeks has recently attained a high intensity.'));
-    this.add(new Speech( 'Winston Churchill', 'If at first all the States of Europe are not willing or able to join the Union, we must nevertheless proceed to assemble and combine those who will and those who can.'));
-    this.add(new Speech( 'Winston Churchill', 'We shall go on to the end. We shall fight in France, we shall fight on the seas and oceans, we shall fight with growing confidence and growing strength in the air...'));
+    this.add(new Speech('Winston Churchill', 'You ask, what is our aim? I can answer in one word: Victory. Victory at all costs—Victory in spite of all terror—Victory, however long and hard the road may be, for without victory there is no survival.'));
+    this.add(new Speech('Winston Churchill', '[The House of Lords] regards all our liberties and political rights as enjoyed and enjoyable only so long as they choose to let us go on having them. But once we touch reality, once we touch their interests and privileges - [kicks his platform] Out!'));
+    this.add(new Speech('Winston Churchill', 'The great air battle which has been in progress over this Island for the last few weeks has recently attained a high intensity.'));
+    this.add(new Speech('Winston Churchill', 'If at first all the States of Europe are not willing or able to join the Union, we must nevertheless proceed to assemble and combine those who will and those who can.'));
+    this.add(new Speech('Winston Churchill', 'We shall go on to the end. We shall fight in France, we shall fight on the seas and oceans, we shall fight with growing confidence and growing strength in the air...'));
     this.add(new Speech('Winston Churchill', 'he day may dawn when fair play, love for one’s fellow-men, respect for justice and freedom, will enable tormented generations to march forth serene and triumphant from the hideous epoch in which we have to dwell. Meanwhile, never flinch, never weary, never despair.'));
     this.add(new Speech('Winston Churchill', 'From Stettin in the Baltic to Trieste in the Adriatic, an iron curtain has descended across the Continent.'));
     this.add(new Speech('Winston Churchill', 'What General Weygand called the Battle of France is over. I expect that the Battle of Britain is about to begin..'));
     this.add(new Speech('Winston Churchill', 'It lies with the Government to satisfy the working classes that there is no justification...er... [long silence] It lies with the Government to satisfy the working classes that there is no justification...er... [long silence] It lies with the Government to satisfy the working classes that there is no justification...er... [long silence]  '));
+
+    
   }
   getAll()
   {
