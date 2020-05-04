28255838d386fd968347b5af0a8ff080deb24ab7
Kate.G <kate.geronimo@didgigo.com.au>
2020-05-04 22:15:05 +0800

    Fix title and updated logo

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SpeechService } from 'src/app/speech/services/speech.service';
+
 
 declare interface RouteInfo {
     path: string;
